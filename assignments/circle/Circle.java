1e33456049b6add430470442b45deda7e9fbd9ce
gabber235 <gabber200@icloud.com>
2020-09-03 12:06:37 +0200

    :sparkles: Added firt assignment

---
+++ assignments/circle/Circle.java
@@ -0,0 +1,21 @@
+/**
+ * Circle
+ * by <<TODO YOUR NAME AND ID HERE>>
+ * and <<TODO YOUR PARTNERS NAME AND ID HERE>>
+ * as group <<TODO GROUP NUMBER HERE>>
+ */
+
+public class Circle {
+
+    public void run() {
+        //TODO implement the program for the assignment
+			
+
+
+        //END TODO
+    }
+
+    public static void main(String[] args) {
+        (new Circle()).run();
+    }
+}
