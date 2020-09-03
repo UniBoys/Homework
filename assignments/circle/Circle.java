0d3c7f4a543b5f74e67ea3ba34a1ffe65fcb8284
gabber235 <gabber200@icloud.com>
2020-09-03 13:59:36 +0200

    :sparkles: Created Basic assingment

--- assignments/circle/Circle.java
+++ assignments/circle/Circle.java
@@ -1,21 +1,76 @@
-/**
- * Circle
- * by <<TODO YOUR NAME AND ID HERE>>
- * and <<TODO YOUR PARTNERS NAME AND ID HERE>>
- * as group <<TODO GROUP NUMBER HERE>>
- */
+package assignments.circle;
 
+import java.util.Scanner;
+/** Circle by Jort van Driel 1579584 and Thijs Aarnoudse 1551159 as group 52 */
 public class Circle {
 
+    private Scanner scanner = new Scanner(System.in);
+
     public void run() {
-        //TODO implement the program for the assignment
-			
+        try {
+            Ring ring1 = readRing();
+            Ring ring2 = readRing();
+            Vector point = readVector();
 
+            boolean pointIsInsideRing1 = ring1.encompassesPoint(point);
+            boolean pointIsInsideRing2 = ring2.encompassesPoint(point);
+            
+            if(pointIsInsideRing1 && pointIsInsideRing2) {
+                System.out.println("The point lies in both circles");
+            } else if(pointIsInsideRing1) {
+                System.out.println("The point lies in the first circle");
+            } else if(pointIsInsideRing2) {
+                System.out.println("he point lies in the second circle");
+            } else {
+                System.out.println("The point does not lie in either circle");
+            }
+        } catch (Exception e) {
+            System.out.println("input error");
+        } 
+    }
 
-        //END TODO
+    private Ring readRing() throws Exception {
+        Vector midpoint = readVector();
+        double radius = scanner.nextDouble();
+
+        if(radius < 0) {
+            throw new Exception();
+        }
+
+        return new Ring(midpoint, radius);
+    }
+
+    private Vector readVector() {
+        double x = scanner.nextDouble();
+        double y = scanner.nextDouble();
+
+        return new Vector(x, y);
     }
 
     public static void main(String[] args) {
         (new Circle()).run();
+    }
+
+    class Vector {
+        final double x, y;
+
+        public Vector(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    class Ring {
+        final Vector midpoint;
+        final double radius;
+
+        public Ring(Vector midpoint, double radius) {
+            this.midpoint = midpoint;
+            this.radius = radius;
+        }
+
+        public boolean encompassesPoint(Vector vector) {
+            return Math.pow(vector.x - midpoint.x, 2) + Math.pow(vector.y - midpoint.y, 2) < Math.pow(radius, 2);
+        }
     }
 }
