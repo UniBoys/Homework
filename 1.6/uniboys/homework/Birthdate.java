36fe83bf33a4e4e2c7ba9d4bf9607b97d776fa78
Jort van Driel <10116429+JortvD@users.noreply.github.com>
2020-09-04 10:51:13 +0200

    Moooore spaaaaaaces

--- 1.6/uniboys/homework/Birthdate.java
+++ 1.6/uniboys/homework/Birthdate.java
@@ -9,86 +9,86 @@ import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 
 class BirthDate {
-  private static Scanner scanner;
-  private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+    private static Scanner scanner;
+    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
 
-  private int calculateAge(final LocalDate dateOfBirth, final LocalDate currentDate) {
-    return Period.between(dateOfBirth, currentDate).getYears();
-  }
-
-  private LocalDate dateFromString(String date) {
-    return LocalDate.parse(date, formatter);
-  }
-
-  private String getCurrentDate() {
-    return LocalDate.now().format(formatter);
-  }
-
-  private LocalDate readDate(String message, String defaultValue) {
-    LocalDate date = null;
-
-    while (date == null) {
-      System.out.format("%s %s(default is %s)%s: ", message, GRAY, defaultValue, RESET);
-      String givenDate = scanner.nextLine();
-
-      if (givenDate.isBlank()) {
-        givenDate = defaultValue;
-        System.out.format(
-            "%s%s%s %s(default is %s)%s: %s%s%s\n",
-            RESET, CURSOR_UP, message, GRAY, defaultValue, RESET, GRAY, defaultValue, RESET);
-      }
-
-      try {
-        date = dateFromString(givenDate);
-      } catch (DateTimeParseException e) {
-        System.out.format(
-            "%s%s %s(default is %s)%s: %s %s(Invalid date)%s\n",
-            CURSOR_UP, message, GRAY, defaultValue, RESET, givenDate, RED, RESET);
-      }
+    private int calculateAge(final LocalDate dateOfBirth, final LocalDate currentDate) {
+        return Period.between(dateOfBirth, currentDate).getYears();
     }
 
-    return date;
-  }
-
-  private void run() {
-    LocalDate dateOfBirth = readDate("What is your birthdate? Format yyyyMMdd", "20020907");
-    LocalDate currentDate = readDate("What is the current date?", getCurrentDate());
-
-    int age = calculateAge(dateOfBirth, currentDate);
-
-    if (age < 0) {
-      System.out.println("You can't be a negative age");
-      run();
-
-      return;
+    private LocalDate dateFromString(String date) {
+        return LocalDate.parse(date, formatter);
     }
 
-    System.out.format("Your age is: %s%s%s\n", age >= 18 ? GREEN : RED, age, RESET);
-  }
-
-  public static void main(final String[] args) {
-    scanner = new Scanner(System.in);
-    final BirthDate birthdate = new BirthDate();
-    birthdate.run();
-  }
-
-  enum ANSICode {
-    RED("\u001b[31m"),
-    GREEN("\u001b[32m"),
-    GRAY("\u001b[38;5;8m"),
-    RESET("\u001b[0m"),
-    CURSOR_UP("\033[1A"),
-    ;
-
-    private String code;
-
-    private ANSICode(String code) {
-      this.code = code;
+    private String getCurrentDate() {
+        return LocalDate.now().format(formatter);
     }
 
-    @Override
-    public String toString() {
-      return code;
+    private LocalDate readDate(String message, String defaultValue) {
+        LocalDate date = null;
+
+        while (date == null) {
+            System.out.format("%s %s(default is %s)%s: ", message, GRAY, defaultValue, RESET);
+            String givenDate = scanner.nextLine();
+
+            if (givenDate.isBlank()) {
+                givenDate = defaultValue;
+                System.out.format(
+                        "%s%s%s %s(default is %s)%s: %s%s%s\n",
+                        RESET, CURSOR_UP, message, GRAY, defaultValue, RESET, GRAY, defaultValue, RESET);
+            }
+
+            try {
+                date = dateFromString(givenDate);
+            } catch (DateTimeParseException e) {
+                System.out.format(
+                        "%s%s %s(default is %s)%s: %s %s(Invalid date)%s\n",
+                        CURSOR_UP, message, GRAY, defaultValue, RESET, givenDate, RED, RESET);
+            }
+        }
+
+        return date;
     }
-  }
+
+    private void run() {
+        LocalDate dateOfBirth = readDate("What is your birthdate? Format yyyyMMdd", "20020907");
+        LocalDate currentDate = readDate("What is the current date?", getCurrentDate());
+
+        int age = calculateAge(dateOfBirth, currentDate);
+
+        if (age < 0) {
+            System.out.println("You can't be a negative age");
+            run();
+
+            return;
+        }
+
+        System.out.format("Your age is: %s%s%s\n", age >= 18 ? GREEN : RED, age, RESET);
+    }
+
+    public static void main(final String[] args) {
+        scanner = new Scanner(System.in);
+        final BirthDate birthdate = new BirthDate();
+        birthdate.run();
+    }
+
+    enum ANSICode {
+        RED("\u001b[31m"),
+        GREEN("\u001b[32m"),
+        GRAY("\u001b[38;5;8m"),
+        RESET("\u001b[0m"),
+        CURSOR_UP("\033[1A"),
+        ;
+
+        private String code;
+
+        private ANSICode(String code) {
+            this.code = code;
+        }
+
+        @Override
+        public String toString() {
+            return code;
+        }
+    }
 }
