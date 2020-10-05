d72e7667ccd009ba91867c44bebea51d0b6d1813
JortvD <10116429+JortvD@users.noreply.github.com>
2020-10-05 17:52:59 +0200

    Change a bit

--- assignments/artist/Painting.java
+++ assignments/artist/Painting.java
@@ -18,36 +18,36 @@ import java.io.*;
 
 public class Painting extends JPanel implements ActionListener {
 
-   /*---- Randomness ----*/
-    /** you can change the variable SEED if you like
-     *  the same program with the same SEED will generate exactly the same sequence of pictures
+    /*---- Randomness ----*/
+    /**
+     * you can change the variable SEED if you like the same program with the same
+     * SEED will generate exactly the same sequence of pictures
      */
     final static long SEED = 37; // seed for the random number generator; any number works
 
     /** do not change the following two lines **/
-    final static Random random = new Random( SEED ); // random generator to be used by all classes
+    final static Random random = new Random(SEED); // random generator to be used by all classes
     int numberOfRegenerates = 0;
 
-   /*---- Screenshots ----
+    /*---- Screenshots ----
     * do not change
     */
     char current = '0';
     String filename = "randomshot_"; // prefix
-    
-   /*---- Dinguses ----*/
-    ArrayList<Dingus> shapes;
-    //...
+
+    /*---- Dinguses ----*/
+    ArrayList<ArrayList<TriangleDingus>> shapes;
 
     public Painting() {
         setPreferredSize(new Dimension(800, 450)); // make panel 800 by 450 pixels.
-        //...
+        // ...
     }
 
     @Override
     protected void paintComponent(Graphics g) { // draw all your shapes
-        super.paintComponent(g);     // clears the panel
-        // draw all shapes
-        // ...
+        super.paintComponent(g); // clears the panel
+        Collections.reverse(shapes);
+        shapes.forEach(a -> a.forEach(s -> s.draw(g)));
     }
 
     /**
@@ -55,54 +55,97 @@ public class Painting extends JPanel implements ActionListener {
      */
     @Override
     public void actionPerformed(ActionEvent e) {
-        if ( "Regenerate".equals(e.getActionCommand()) ) {
+        if ("Regenerate".equals(e.getActionCommand())) {
             regenerate();
             repaint();
         } else { // screenshot
-            saveScreenshot( this, filename+current++ ); // ++ to show of compact code :-)
+            saveScreenshot(this, filename + current++); // ++ to show of compact code :-)
         }
     }
+
+    int TOTAL_STEPS = 20;
+    float STEEPNESS = 2f;
+    float START_AREA;
 
     void regenerate() {
         numberOfRegenerates++; // do not change
-        
-        // clear the shapes list
-        // ...
 
-        // create random shapes
-        // ...
+        shapes = new ArrayList<>();
+
+        float radius = random.nextFloat() * 20 + 40;
+        float rotation = random.nextFloat() * (2f / 3f) * (float) Math.PI;
+
+        TriangleDingus centerTriangle = new TriangleDingus(getSize().width / 2, getSize().height / 2, rotation, radius);
+        shapes.add(new ArrayList<>(Arrays.asList(centerTriangle)));
+
+        START_AREA = 0.75f * (float) Math.sqrt(3d) * (float) Math.pow(radius, 2);
+
+        float[][][] children = centerTriangle.getNextStartingCords();
+
+        for (float[][] child : children) {
+            generateShapes(child[0], child[1], child[2], 0);
+        }
     }
 
-    /** 
-     * saves a screenshot of a Component on disk
-     *  overides existing files
+    void generateShapes(float[] corner0, float[] corner1, float[] corner2, int step) {
+        float decrease = (1f / ((STEEPNESS / TOTAL_STEPS) * (float) Math.pow((double)(step + 1f), 2d) + 1f));
+        float randomness = random.nextFloat() * 0.04f + 0.98f;
+        float area = START_AREA * decrease * randomness;
+
+        if (area < 0 || step >= TOTAL_STEPS) {
+            if(area < 0) System.out.println("to small");
+            return;
+        }
+
+        TriangleDingus dingus = new TriangleDingus(area, corner0, corner1, corner2);
+
+        for(ArrayList<TriangleDingus> list : shapes) {
+            for(TriangleDingus triangle : list) {
+                if(triangle.isInside(dingus.getCorner(2))) {
+                    return;
+                }
+            }
+        }
+
+        if(shapes.size() == step) {
+            shapes.add(new ArrayList<>());
+        }
+
+        shapes.get(step).add(dingus);
+
+        float[][][] children = dingus.getNextStartingCords();
+
+        for (float[][] child : children) {
+            generateShapes(child[0], child[1], child[2], ++step);
+        }
+    }
+
+    /**
+     * saves a screenshot of a Component on disk overides existing files
      *
      * @param component - Component to be saved
-     * @param name - filename of the screenshot, followed by a sequence number
+     * @param name      - filename of the screenshot, followed by a sequence number
      * 
-     * do not change
+     *                  do not change
      */
     void saveScreenshot(Component component, String name) {
-        String randomInfo = ""+SEED+"+"+ (numberOfRegenerates-1); //minus 1 because the initial picture should not count
-        System.out.println( SwingUtilities.isEventDispatchThread() );
-        BufferedImage image =
-            new BufferedImage(
-                              component.getWidth(),
-                              component.getHeight(),
-                              BufferedImage.TYPE_INT_RGB
-                             );
+        String randomInfo = "" + SEED + "+" + (numberOfRegenerates - 1); // minus 1 because the initial picture should
+                                                                         // not count
+        System.out.println(SwingUtilities.isEventDispatchThread());
+        BufferedImage image = new BufferedImage(component.getWidth(), component.getHeight(),
+                BufferedImage.TYPE_INT_RGB);
         // call the Component's paint method, using
         // the Graphics object of the image.
         Graphics graphics = image.getGraphics();
-        component.paint( graphics ); // alternately use .printAll(..)
+        component.paint(graphics); // alternately use .printAll(..)
         graphics.drawString(randomInfo, 0, component.getHeight());
-        
+
         try {
-            ImageIO.write(image, "PNG", new File(name+".png"));
-            System.out.println( "Saved screenshot as "+ name );
-        } catch( IOException e ) {
-            System.out.println( "Saving screenshot failed: "+e );
+            ImageIO.write(image, "PNG", new File(name + ".png"));
+            System.out.println("Saved screenshot as " + name);
+        } catch (IOException e) {
+            System.out.println("Saving screenshot failed: " + e);
         }
     }
-    
+
 }
--- assignments/artist/TriangleDingus.java
+++ assignments/artist/TriangleDingus.java
@@ -9,32 +9,113 @@ class TriangleDingus extends Dingus {
     private float[] corner2;
     private float[] corner3;
 
-    public TriangleDingus(int area, float[] corner1, float[] corner2, boolean positive) {
-        super(0, 0);
+    public TriangleDingus(int x, int y, float rotation, float radius) {
+        super(1, 1);
 
+        float[] center = new float[] { x, y };
+
+        this.corner1 = calcCorner(center, radius, rotation, 0f);
+        this.corner2 = calcCorner(center, radius, rotation, 1f);
+        this.corner3 = calcCorner(center, radius, rotation, 2f);
+    }
+
+    public TriangleDingus(float area, float[] corner0, float[] corner1, float[] corner2) {
+        super(1, 1);
+
+        this.corner1 = corner1;
         this.corner2 = corner2;
-        this.corner3 = corner3;
 
+        // The length of the vector from corner1 to corner2 of the original triangle.
         float length = length(corner1, corner2);
-
-        float betweenPercentage = random.nextFloat();
+        // The percentage of the vector parallel to the vector of corner1 to corner2,
+        // which indicates where corner3 will lie.
+        float betweenPercentage = random.nextFloat() * .6f + .3f;
+        // The height of our new vector.
         float height = 2f * area / length;
-        float betweenX = betweenPercentage * (corner1[0] - corner2[0]) + corner1[0];
-        float betweenY = betweenPercentage * (corner1[1] - corner2[1]) + corner1[1];
-        float heightX = y / length * height;
-        float heightY = -x / length * height;
-        corner3 = new float[] {
-            positive ? betweenX + heightX : betweenX - heightX, 
-            positive ? betweenY + heightY : betweenY - heightY
-        };
+        // The x coordinate on the vector from corner1 to corner2 parallel to which our
+        // new corner will lie.
+        float betweenX = betweenPercentage * (corner2[0] - corner1[0]) + corner1[0];
+        // The y coordinate on the vector from corner1 to corner2 parallel to which our
+        // new corner will lie.
+        float betweenY = betweenPercentage * (corner2[1] - corner1[1]) + corner1[1];
+        // The height x coordinate that will be added to the between x to form our
+        // corner x coordinate.
+        float heightX = (corner2[1] - corner1[1]) / length * height;
+        // The height y coordinate that will be added to the between y to form our
+        // corner y coordinate.
+        float heightY = -(corner2[0] - corner1[0]) / length * height;
+        // If our coordinate should lie at the postive or negative side of the vector
+        // from corner1 to corner2, determined by which point is furthest away from the
+        // remaining corner of the original triangle.
+        boolean positive = length(corner0, new float[] { betweenX + heightX, betweenY + heightY }) > length(corner0,
+                new float[] { betweenX - heightX, betweenY - heightY });
+        // The coordinates for corner3, created by adding or subtracting the between
+        // coordinates with the height coordinates.
+        corner3 = new float[] { positive ? betweenX + heightX : betweenX - heightX,
+                positive ? betweenY + heightY : betweenY - heightY };
     }
 
     float length(float[] a, float[] b) {
-        return (float) Math.sqrt(Math.pow(a[0]-b[0], 2) + Math.pow(a[1] - b[1], 2));
+        return (float) Math.sqrt(Math.pow(a[0] - b[0], 2) + Math.pow(a[1] - b[1], 2));
     }
 
     @Override
     void draw(Graphics g) {
+        g.drawPolygon(getCord(0), getCord(1), 3);
+    }
 
+    int[] getCord(int index) {
+        return new int[] { round(corner1[index]), round(corner2[index]), round(corner3[index]) };
+    }
+
+    int round(float number) {
+        return Math.round(number);
+    }
+
+    float[][][] getNextStartingCords() {
+        return new float[][][] { { corner1, corner2, corner3 }, { corner2, corner1, corner3 } };
+    }
+
+    float[] calcCorner(float[] center, float radius, float rotation, float number) {
+        float angle = (float) number * (2f / 3f) * (float) Math.PI + rotation;
+
+        return new float[] { (float) (center[0] + Math.cos(angle) * radius),
+                (float) (center[1] + Math.sin(angle) * radius) };
+    }
+
+    float[] getCorner(int n) {
+        switch(n) {
+            case 0:
+                return corner1;
+            case 1:
+                return corner2;
+            case 2:
+                return corner3;
+        }
+
+        return null;
+    }
+
+    boolean isInside(float[] point) {
+        double dx = point[0] - corner3[0];
+        double dy = point[1] - corner3[1];
+        double det = (corner2[1] - corner3[1]) * (corner1[0] - corner3[0]) - (corner3[0] - corner2[0]) * (corner3[1] - corner1[1]);
+        double a = (corner2[1] - corner3[1]) * dx + (corner3[0] - corner2[0]) * dy;
+
+        if (a < Math.min(det, 0) || a > Math.max(det, 0)) {
+            return false;
+        }
+
+        double b = (corner3[1] - corner1[1]) * dx + (corner1[0] - corner3[0]) * dy;
+
+        if (b < Math.min(det, 0) || b > Math.max(det, 0))
+            return false;
+
+        double c = det - a - b;
+
+        if (c < Math.min(det, 0) || c > Math.max(det, 0))
+            return false;
+
+        return true;
     }
 }
